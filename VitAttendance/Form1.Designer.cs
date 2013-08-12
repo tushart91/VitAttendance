3d9baa7b2a572e146ed7f6df7a74afbcdfefcccf
Tushar Tiwari <tushart91@gmail.com>
2013-08-12 09:33:26 +0530

    Init commit

---
+++ VitAttendance/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace VitAttendance
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.submit = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.reset = new System.Windows.Forms.Button();
+            this.dropDown = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(273, 12);
+            this.textBox1.MaxLength = 9;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(187, 27);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(164, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Registration No";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 50);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(786, 513);
+            this.webBrowser1.TabIndex = 2;
+            this.webBrowser1.Visible = false;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // submit
+            // 
+            this.submit.Location = new System.Drawing.Point(466, 14);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(75, 23);
+            this.submit.TabIndex = 3;
+            this.submit.Text = "Submit";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.submit_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(639, 14);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(128, 22);
+            this.progressBar1.TabIndex = 4;
+            this.progressBar1.Visible = false;
+            // 
+            // reset
+            // 
+            this.reset.Location = new System.Drawing.Point(547, 14);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(75, 23);
+            this.reset.TabIndex = 5;
+            this.reset.Text = "Reset";
+            this.reset.UseVisualStyleBackColor = true;
+            this.reset.Click += new System.EventHandler(this.reset_Click);
+            // 
+            // dropDown
+            // 
+            this.dropDown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.dropDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dropDown.FormattingEnabled = true;
+            this.dropDown.Items.AddRange(new object[] {
+            "Attendance",
+            "Marks"});
+            this.dropDown.Location = new System.Drawing.Point(18, 12);
+            this.dropDown.Name = "dropDown";
+            this.dropDown.Size = new System.Drawing.Size(140, 24);
+            this.dropDown.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.submit;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(786, 563);
+            this.Controls.Add(this.dropDown);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.submit);
+            this.Controls.Add(this.webBrowser1);
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(0, 50, 0, 0);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VIT AMSTAT";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button submit;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button reset;
+        private System.Windows.Forms.ComboBox dropDown;
+    }
+}
+
---
+++ VitAttendance/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace VitAttendance
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" || dropDown.SelectedItem != null)
+            {
+                webBrowser1.Visible = false;
+                webBrowser1.Navigate("https://academics.vit.ac.in/parent/parent_login.asp");
+                textBox1.Text = textBox1.Text.ToUpper();
+                webBrowser1.DocumentCompleted -= new WebBrowserDocumentCompletedEventHandler(webBrowser1_DocumentCompleted5);
+                webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(webBrowser1_DocumentCompleted);
+                progressBar1.PerformStep();
+                progressBar1.Visible = true;
+            }
+            else if(dropDown.SelectedItem == null)
+                MessageBox.Show("Please Select a Action from the DropDown", "Error", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            else
+                MessageBox.Show("Enter a Registration Number", "Error", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (!webBrowser1.IsBusy)
+            {
+                webBrowser1.Visible = false;
+                progressBar1.PerformStep();
+                HtmlElementCollection ele = webBrowser1.Document.GetElementsByTagName("input");
+                ele[1].SetAttribute("value", textBox1.Text);
+                HtmlElementCollection font;
+                font = webBrowser1.Document.GetElementsByTagName("font");
+                string captcha = font[5].InnerText;
+                captcha = captcha.Trim();
+                string temp = captcha.Replace(" ", string.Empty);
+
+                ele[2].SetAttribute("value", temp);
+
+                HtmlElementCollection form = webBrowser1.Document.Forms;
+                form[0].InvokeMember("submit");
+                progressBar1.PerformStep();
+                webBrowser1.DocumentCompleted -= new WebBrowserDocumentCompletedEventHandler(webBrowser1_DocumentCompleted);
+                webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(webBrowser1_DocumentCompleted2);
+            }
+        }
+
+        private void webBrowser1_DocumentCompleted2(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            webBrowser1.Visible = false;
+            progressBar1.PerformStep();
+            if(dropDown.SelectedIndex == 0)
+                webBrowser1.Navigate("https://academics.vit.ac.in/parent/attn_report.asp");
+            else if (dropDown.SelectedIndex == 1)
+                webBrowser1.Navigate("https://academics.vit.ac.in/parent/marks.asp");
+            webBrowser1.DocumentCompleted -= new WebBrowserDocumentCompletedEventHandler(webBrowser1_DocumentCompleted2);
+            webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(webBrowser1_DocumentCompleted3);
+            progressBar1.PerformStep();
+            if (dropDown.SelectedIndex == 1)
+                progressBar1.Value = 100;
+        }
+
+        private void webBrowser1_DocumentCompleted3(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (dropDown.SelectedIndex == 1)
+            {
+                webBrowser1.Visible = true;
+                progressBar1.Value = 0;
+                progressBar1.Visible = false;
+                webBrowser1.DocumentCompleted -= new WebBrowserDocumentCompletedEventHandler(webBrowser1_DocumentCompleted3);
+                webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(webBrowser1_DocumentCompleted5);
+                return;
+            }
+            webBrowser1.Visible = false;
+            progressBar1.PerformStep();
+            HtmlElementCollection date = webBrowser1.Document.GetElementsByTagName("input");
+            date[0].SetAttribute("value", "02-Jan-2012");
+            HtmlElement toDate = webBrowser1.Document.GetElementById("to_date");
+            date[1].SetAttribute("value", DateTime.Now.ToString("dd-MMM-yyyy"));
+            date[2].InvokeMember("Click");
+            webBrowser1.DocumentCompleted -= new WebBrowserDocumentCompletedEventHandler(webBrowser1_DocumentCompleted3);
+            webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(webBrowser1_DocumentCompleted4);
+            progressBar1.PerformStep();
+        }
+
+        private void webBrowser1_DocumentCompleted4(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            webBrowser1.Visible = true;
+            progressBar1.Visible = false;
+            progressBar1.Value = 0;
+            webBrowser1.Document.Cookie.Remove(0, webBrowser1.Document.Cookie.Length);
+            webBrowser1.DocumentCompleted -= new WebBrowserDocumentCompletedEventHandler(webBrowser1_DocumentCompleted4);
+            webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(webBrowser1_DocumentCompleted5);
+        }
+
+        private void webBrowser1_DocumentCompleted5(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void reset_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Visible = false;
+            progressBar1.Visible = false;
+            progressBar1.Value = 0;
+            textBox1.SelectAll();
+            webBrowser1.Document.Cookie.Remove(0, webBrowser1.Document.Cookie.Length);
+            webBrowser1.DocumentCompleted -= new WebBrowserDocumentCompletedEventHandler(webBrowser1_DocumentCompleted5);
+            webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(webBrowser1_DocumentCompleted);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dropDown.SelectedIndex = 0;
+        }
+    }
+}
